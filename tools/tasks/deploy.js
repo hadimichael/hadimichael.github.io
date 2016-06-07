fac2e6b2d6bfc1de3f04346defa84ff37c24c845
Hadi Michael <hadimichael@gmail.com>
2016-06-07 22:00:18 +1000

    Deploy into multiple repos

--- tools/tasks/deploy.js
+++ tools/tasks/deploy.js
@@ -4,14 +4,35 @@ const gutil = require('gulp-util');
 
 const config = require('./../config');
 
+const appRepoDeploymentOptions = {
+	landing: {
+		branch: 'master', //use default remoteUrl, which is the current repo
+	},
+	notes: {
+		remoteUrl: 'https://github.com/hadimichael/notes.git', //use default branch, which is gh-pages
+	},
+};
+
+if (!appRepoDeploymentOptions[process.env.APP]) {
+	gutil.log(gutil.colors.red('[deploy] Unknown APP. ' +
+		`Expected [${Object.keys(appRepoDeploymentOptions).toString()}], but got ${process.env.APP}`));
+	throw new Error(`Expected a valid APP environment variable, got: ${process.env.APP}`);
+}
+
+const ghPagesDefaultOptions = {
+	message: `Build and deploy on ${new Date()}`,
+	logger: (message) => {
+		gutil.log(gutil.colors.blue('[deploy]'), message);
+	},
+};
+
+const ghPagesOptions = Object.assign(
+	ghPagesDefaultOptions,
+	appRepoDeploymentOptions[process.env.APP]
+);
+
 gulp.task('deploy', ['dist'], () => {
-	return ghpages.publish(config.paths.dist, {
-		message: `Build and deploy on ${new Date()}`,
-		branch: 'master',
-		logger: (message) => {
-			gutil.log(gutil.colors.blue('[deploy]'), message);
-		},
-	}, (err) => {
+	return ghpages.publish(config.paths.dist, ghPagesOptions, (err) => {
 		if (!!err) {
 			gutil.log(gutil.colors.red('[deploy] Error deploying'), err);
 		}
